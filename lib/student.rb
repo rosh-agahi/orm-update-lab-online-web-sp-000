82fba74d45e9ee1feb9728916c49244339179055
Roshanak Agahi <rosh.agahi@gmail.com>
2019-10-09 18:39:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,14 @@ class Student
     end
   end
   
-  
+  def update
+    sql = <<-SQL
+      UPDATE students 
+      SET name = ?, grade = ?
+      WHERE id = ?
+      SQL
+    DB[:conn].execute(sql,self.name,self.grade,self.id)
+  end
     
     
 end
